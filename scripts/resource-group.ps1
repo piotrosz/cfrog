0574ba392438d466cabbd191573e19da02f6a4e7
Piotr Ludwiczuk <piotrosz@gmail.com>
2024-11-22 21:44:25 +0100

    home is recipe list

--- scripts/resource-group.ps1
+++ scripts/resource-group.ps1
@@ -7,6 +7,7 @@ $storageAccountName = "stor0cfrog"
 $location = "polandcentral"
 $subscription = "311d0bf6-7b5c-40ae-880d-a81a7407ebf1"
 $appPlanName = "appplan-cfrog"
+$webAppName = "webapp-cfrog"
 
 az account set --subscription $subscription
 
@@ -27,4 +28,10 @@ az keyvault create --name "kvcfrog" --resource-group $resourceGroupName
 az appservice plan create --name $appPlanName --resource-group $resourceGroupName --sku B1 --is-linux --location $location
 
 # Create app service
-az webapp create --name "webapp-cfrog" --resource-group $resourceGroupName --plan $appPlanName --runtime "DOTNETCORE:8.0"
+az webapp create --name $webAppName --resource-group $resourceGroupName --plan $appPlanName --runtime "DOTNETCORE:8.0"
+
+# Assign idenity
+az webapp identity assign -g $resourceGroupName -n $webAppName
+
+az webapp identity show -g $resourceGroupName -n $webAppName
+
