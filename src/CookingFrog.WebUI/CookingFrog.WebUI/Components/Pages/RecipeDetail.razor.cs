ba6c18df2ba66c26eda39e61e5d7fec99057e847
Piotr Ludwiczuk <piotrosz@gmail.com>
2024-12-26 14:28:16 +0100

    extract TitleEdit.razor

--- src/CookingFrog.WebUI/CookingFrog.WebUI/Components/Pages/RecipeDetail.razor.cs
+++ src/CookingFrog.WebUI/CookingFrog.WebUI/Components/Pages/RecipeDetail.razor.cs
@@ -5,10 +5,6 @@ namespace CookingFrog.WebUI.Components.Pages;
 
 public partial class RecipeDetail
 {
-    private bool _isSummaryEditMode;
-
-    private string _recipeSummaryForEdit = string.Empty;
-    
     private Recipe? Recipe { get; set; }
 
     protected override async Task OnInitializedAsync()
@@ -16,25 +12,18 @@ public partial class RecipeDetail
         if (Guid is not null && System.Guid.TryParse(Guid, out var guid))
         {
             Recipe = await RecipesReadRepo.GetRecipe(guid);
-            _recipeSummaryForEdit = Recipe.Summary;
         }
     }
 
     [Parameter] 
     public string? Guid { get; set; }
-
-    private void SummaryEdit()
+    
+    private async Task UpdateSummary(string? newSummary)
     {
-        _isSummaryEditMode = true;
-    }
-
-    private async Task UpdateSummary()
-    {
-        if (Recipe is not null && !string.IsNullOrWhiteSpace(_recipeSummaryForEdit))
+        if (Recipe is not null && !string.IsNullOrWhiteSpace(newSummary))
         {
-            await RecipesUpdateRepo.UpdateTitle(_recipeSummaryForEdit, Recipe.Guid, CancellationToken.None);
+            await RecipesUpdateRepo.UpdateTitle(newSummary, Recipe.Guid, CancellationToken.None);
             Recipe = await RecipesReadRepo.GetRecipe(Recipe.Guid);
         }
-        _isSummaryEditMode = false;
     }
 }
