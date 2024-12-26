4f272bbbb957ba108d4f0108454d0c34b343bb1f
Piotr Ludwiczuk <piotrosz@gmail.com>
2024-12-26 12:14:13 +0100

    Update title in progress

--- src/CookingFrog.WebUI/CookingFrog.WebUI/Components/Pages/RecipeDetail.razor.cs
+++ src/CookingFrog.WebUI/CookingFrog.WebUI/Components/Pages/RecipeDetail.razor.cs
@@ -5,15 +5,32 @@ namespace CookingFrog.WebUI.Components.Pages;
 
 public partial class RecipeDetail
 {
+    private bool _isSummaryEditMode;
+
+    private string _recipeSummaryForEdit;
+    
     private Recipe? Recipe { get; set; }
 
     protected override async Task OnInitializedAsync()
     {
-        if (Guid is not null)
+        if (Guid is not null && System.Guid.TryParse(Guid, out var guid))
         {
-            Recipe = await RecipesRepo.GetRecipe(System.Guid.Parse(Guid));
+            Recipe = await RecipesRepo.GetRecipe(guid);
+            _recipeSummaryForEdit = Recipe.Summary;
         }
     }
 
-    [Parameter] public string? Guid { get; set; }
+    [Parameter] 
+    public string? Guid { get; set; }
+
+    private void SummaryEdit()
+    {
+        _isSummaryEditMode = true;
+    }
+
+    private void UpdateSummary()
+    {
+        _isSummaryEditMode = false;
+        // TODO: Save 
+    }
 }
