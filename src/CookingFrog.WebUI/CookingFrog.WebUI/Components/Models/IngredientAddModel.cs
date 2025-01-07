4f152aff5b3334d2956b66c66dacd7df030e32cc
Piotr Ludwiczuk <piotrosz@gmail.com>
2025-01-08 00:17:00 +0100

    modal in progress

---
+++ src/CookingFrog.WebUI/CookingFrog.WebUI/Components/Models/IngredientAddModel.cs
@@ -0,0 +1,10 @@
+using CookingFrog.Domain;
+
+namespace CookingFrog.WebUI.Components.Models;
+
+public record IngredientAddModel(string Name, decimal Quantity, UnitEnum Unit)
+{
+    public string Name { get; set; } = Name;
+    public decimal Quantity { get; set; } = Quantity;
+    public UnitEnum Unit { get; set; } = Unit;
+};
