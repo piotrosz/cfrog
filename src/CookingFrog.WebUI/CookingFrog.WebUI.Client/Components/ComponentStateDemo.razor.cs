1e72a2f734e7689c5232711ced36ce499ee1ee68
Piotr Ludwiczuk <piotrosz@gmail.com>
2025-02-13 20:58:22 +0100

    ComponentStateDemo.razor

---
+++ src/CookingFrog.WebUI/CookingFrog.WebUI.Client/Components/ComponentStateDemo.razor.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Components.Web;
+
+namespace CookingFrog.WebUI.Client.Components;
+
+public partial class ComponentStateDemo
+{
+    private string _rendererLocation = string.Empty;
+    private string _renderMode = string.Empty;
+
+    protected override void OnInitialized()
+    {
+        _rendererLocation = RendererInfo.Name switch
+        {
+            "Static" => "Server (Static)",
+            "Server" => "Server (Interactive)",
+            "WebAssembly" => "Client (WebAssembly)",
+            "WebView" => "Native (WebView)",
+            _ => "Unknown"
+        };
+
+        _renderMode = AssignedRenderMode switch
+        {
+            null => "Static server",
+            InteractiveServerRenderMode => "Interactive server",
+            InteractiveWebAssemblyRenderMode => "Interactive WebAssembly",
+            InteractiveAutoRenderMode => "Interactive Auto",
+            _ => "Unknown"
+        };
+    }
+}
